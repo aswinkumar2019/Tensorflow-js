3a539cf9a5e7a67cc4dd647c99b2907df41f5460
Aswin Kumar <aswinkumar2019@gmail.com>
2020-04-08 00:19:28 +0530

    Add files via upload

---
+++ face.js
@@ -0,0 +1,164 @@
+class App extends React.Component {
+  // reference to both the video and canvas
+  videoRef = React.createRef();
+  canvasRef = React.createRef();
+
+  // we are gonna use inline style
+  styles = {
+    position: 'fixed',
+    top: 150,
+    left: 150,
+  };
+
+
+  detectFromVideoFrame = (model, video) => {
+    model.estimateFaces(video, "False").then(predictions => {
+      this.showDetections(predictions);
+    /*  var count;
+      var classes_distinct;
+      console.log('Total number of Objects',predictions.length);
+      var classes = []
+      for(count=0;count<predictions.length;count++){
+        classes[count] = predictions[count].class
+       }
+       classes_distinct = [...new Set(classes)];
+       console.log('count',classes.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {}))
+       console.log('Distinct classes',classes_distinct)*/
+       console.log('finish')
+
+      console.log(predictions)
+      requestAnimationFrame(() => {
+        this.detectFromVideoFrame(model, video);
+      });
+    }, (error) => {
+      console.log("Couldn't start the webcam")
+      console.error(error)
+    });
+  };
+
+  showDetections = predictions => {
+    const ctx = this.canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    const font = "24px helvetica";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+
+    predictions.forEach(prediction => {
+        if (predictions.length > 0) {
+    /*
+    `predictions` is an array of objects describing each detected face, for example:
+ 
+    [
+      {
+        topLeft: [232.28, 145.26],
+        bottomRight: [449.75, 308.36],
+        probability: [0.998],
+        landmarks: [
+          [295.13, 177.64], // right eye
+          [382.32, 175.56], // left eye
+          [341.18, 205.03], // nose
+          [345.12, 250.61], // mouth
+          [252.76, 211.37], // right ear
+          [431.20, 204.93] // left ear
+        ]
+      }
+    ]
+    */
+ 
+    for (let i = 0; i < predictions.length; i++) {
+      const start = predictions[i].topLeft;
+      const end = predictions[i].bottomRight;
+      const size = [end[0] - start[0], end[1] - start[1]];
+ 
+      // Render a rectangle over each detected face.
+      ctx.fillRect(start[0], start[1], size[0], size[1]);
+    }
+  }
+}
+ 
+ 
+     /* const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      const width = prediction.bbox[2];
+      const height = prediction.bbox[3];
+      // Draw the bounding box.
+      ctx.strokeStyle = "#2fff00";
+      ctx.lineWidth = 1;
+      ctx.strokeRect(x, y, width, height);
+      // Draw the label background.
+      ctx.fillStyle = "#2fff00";
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10);
+      // draw top left rectangle
+      ctx.fillRect(x, y, textWidth + 10, textHeight + 10);
+      // draw bottom left rectangle
+      ctx.fillRect(x, y + height - textHeight, textWidth + 15, textHeight + 10);
+
+      // Draw the text last to ensure it's on top.
+      ctx.fillStyle = "#000000";
+      ctx.fillText(prediction.class, x, y);
+      ctx.fillText(prediction.score.toFixed(2), x, y + height - textHeight);
+    */
+});
+  };
+
+  componentDidMount() {
+    if (navigator.mediaDevices.getUserMedia || navigator.mediaDevices.webkitGetUserMedia) {
+      // define a Promise that'll be used to load the webcam and read its frames
+      const webcamPromise = navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+          audio: false,
+        })
+        .then(stream => {
+          // pass the current frame to the window.stream
+          window.stream = stream;
+          // pass the stream to the videoRef
+          this.videoRef.current.srcObject = stream;
+
+          return new Promise(resolve => {
+            this.videoRef.current.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        }, (error) => {
+          console.log("Couldn't start the webcam")
+          console.error(error)
+        });
+
+      // define a Promise that'll be used to load the model
+      const loadlModelPromise = blazeface.load();
+      const returnTensors = false;
+      
+      // resolve all the Promises
+      Promise.all([loadlModelPromise, webcamPromise])
+        .then(values => {
+          this.detectFromVideoFrame(values[0], this.videoRef.current);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+
+  // here we are returning the video frame and canvas to draw,
+  // so we are in someway drawing our video "on the go"
+  render() {
+    return (
+      <div> 
+        <video
+          style={this.styles}
+          autoPlay
+          muted
+          ref={this.videoRef}
+          width="720"
+          height="600"
+        />
+        <canvas style={this.styles} ref={this.canvasRef} width="720" height="650" />
+      </div>
+    );
+  }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(React.createElement(App), domContainer);
